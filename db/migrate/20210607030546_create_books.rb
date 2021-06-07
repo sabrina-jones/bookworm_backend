6adc855e57a50799de76be50c4f2e14e7a9b786d
sabrina-jones <sabrina.jones87@gmail.com>
2021-06-06 22:21:40 -0500

    book model

---
+++ db/migrate/20210607030546_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.string :image_url
+      t.string :overview
+      t.integer :category_id
+
+      t.timestamps
+    end
+  end
+end
